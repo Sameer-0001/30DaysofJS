268a3e27a38ae61a3c11250d4c95835ec493666d
Sameer-0001 <123102173@nitkkr.ac.in>
2024-07-20 16:27:21 +0530

    Create day2.js
    This is 3rd program of github using javascript

---
+++ day2.js
@@ -0,0 +1,71 @@
+// Task 1
+let m=9;
+let n=1;
+console.log(m+n); // 10
+
+// Task 2
+let p=20;
+let q=5;
+console.log(p-q); // 15
+
+//Task 3
+let a=2;
+let b=8;
+console.log(a*b); // 16
+
+//Task 4
+let c=50;
+let d=10;
+console.log(c/d); // 5
+
+//Task 5
+let i=60;
+let j=7;
+console.log(i%j); // 4
+
+//Task 6
+let k=1;
+k+=2;
+console.log(k); // 3
+
+//Task 7
+let l=8;
+l-=1;
+console.log(l); // 7
+
+//Task 8
+console.log(8>5); // true
+console.log(8<5); // false
+
+//Task 9
+console.log(9>=9); // true
+console.log(4<=3); // false
+
+//Task 10
+let t=12;
+let r="12";
+console.log(t==r);  // true
+console.log(t===r); // false
+
+//Task 11
+let v=56;
+while(v>0 && v%2==0){
+    v=v/2;}
+console.log(i); // 7
+
+//Task 12
+let age=21;
+if(age==3 || age>3){
+    console.log("eligible for school admission")}
+else{
+    console.log("underage not eligible");}
+
+//Task 13
+if(!false){
+    console.log("stmt executed");}
+
+//Task 14
+let num=-19;
+console.log(num>=0 ? "positive" : "negative"); //Output:Negative
+
+
